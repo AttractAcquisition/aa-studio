0aad501ad0f4b6bd21398cac6a11973d3bf8d260
AWTMedia <alexander.thomas2401@gmail.com>
2025-12-24 21:53:46 +0200

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -25,7 +25,6 @@ export default function Dashboard() {
   const { contentItems, isLoading: isLoadingContent } = useContentItems();
   const [addProofOpen, setAddProofOpen] = useState(false);
 
-  
   const kpis = [
     { 
       title: "Posts Created", 
