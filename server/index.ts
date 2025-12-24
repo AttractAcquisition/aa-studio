f3ed40cd3f959c05f7d7a2b4cdac1e77f63331e7
AWTMedia <alexander.thomas2401@gmail.com>
2025-12-24 13:32:21 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
 import express from "express";
 import cors from "cors";
 import { createClient } from "@supabase/supabase-js";
